bf401ea87e295e5ce3b4a77fb3b3ebf977c14dfe
hgminerva <hgminerva@gmail.com>
2015-02-26 10:45:58 -0800

    Summary Average Update

--- MagentaTrader/Data/MagentaTradersDB.designer.cs
+++ MagentaTrader/Data/MagentaTradersDB.designer.cs
@@ -2262,7 +2262,7 @@ namespace MagentaTrader.Data
 			OnCreated();
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", DbType="Int NOT NULL", IsPrimaryKey=true)]
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
 		public int Id
 		{
 			get
